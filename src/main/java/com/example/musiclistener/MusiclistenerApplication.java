8063bc95db324156976ec215ab0c706a929ec841
rustamov-bite <assasins_murza006@mail.ru>
2021-01-21 09:46:00 +0600

    Init project

---
+++ src/main/java/com/example/musiclistener/MusiclistenerApplication.java
@@ -0,0 +1,13 @@
+package com.example.musiclistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusiclistenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusiclistenerApplication.class, args);
+    }
+
+}
